cb76454d03563e26ae13cdb1607083355b1706d9
Micah Silverman <micah@afitnerd.com>
2019-10-22 12:00:55 -0400

    fixed console output

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 console.log('...building...');
-console.log('Client ID: ' + process.env.OKTA_OAUTH2_CLIENT_ID);
+console.log('Client ID: ' + process.env.OKTA_OAUTH2_CLIENT_ID_SPA);
 console.log('Okta Org Url: ' + process.env.OKTA_CLIENT_ORGURL);
 process.env.VUE_APP_OKTA_OAUTH2_CLIENT_ID_SPA=process.env.OKTA_OAUTH2_CLIENT_ID_SPA;
 process.env.VUE_APP_OKTA_CLIENT_ORGURL=process.env.OKTA_CLIENT_ORGURL;
